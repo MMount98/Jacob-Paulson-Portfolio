4707a98fd733b0a25ed009a345e15fff4d2bbc9b
Michael Mount <mount.photos@gmail.com>
2023-05-04 14:06:32 -0600

    Added fontAwseome Icons to Nav plus tested links

--- jacob-paulson/src/components/NavBar.js
+++ jacob-paulson/src/components/NavBar.js
@@ -8,7 +8,20 @@ export default function Sidebar() {
         <div className="btn btn-ghost normal-case text-xl">Jacob Paulson</div>
       </div>
       <div className="navbar-end">
-        <Link to="#" className="btn btn-outline mr-6">
+        <Link to="https://www.instagram.com/_jacobpaulson_/">
+          <i className="fa-brands fa-instagram m-2 lg:m-3"></i>
+        </Link>
+        <Link to="https://www.tiktok.com/@jacobryanpaulson">
+          <i className="fa-brands fa-tiktok m-2 lg:m-3"></i>
+        </Link>
+        <Link to="https://twitter.com/JRPaulyStreams">
+          <i className="fa-brands fa-twitter m-2 lg:m-3"></i>
+        </Link>
+
+        <Link
+          to="https://www.tiktok.com/@jacobryanpaulson"
+          className="btn btn-outline lg:mr-6 "
+        >
           Contact Me!
         </Link>
       </div>
